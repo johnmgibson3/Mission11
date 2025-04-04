6887c91ab4c2f3a7fbce28ead81f17cd43ed322b
John Gibson <johnmgibson3@gmail.com>
2025-04-04 12:07:37 -0600

    changes

--- Frontend/src/pages/BooksPage.tsx
+++ Frontend/src/pages/BooksPage.tsx
@@ -8,24 +8,27 @@ function BooksPage() {
   const [selectedCategories, setSelectedCategories] = useState<string[]>([]);
 
   return (
-    <div
-      className="row align-items-stretch"
-      style={{ backgroundColor: '#f0f0f0' }}
-    >
-      <div className="container-fluid mt-4 px-5">
+    <div className="bg-light min-vh-100 py-4">
+      <div className="container-fluid px-4 px-md-5">
         <CartSummary />
         <WelcomeBand />
-        <div className="row align-items-stretch">
-          <div className="col-md-4 col-lg-3 d-flex">
-            <div className="w-100">
+
+        <div className="row mt-4">
+          {/* Category Filter Column */}
+          <div className="col-md-4 col-lg-3 mb-4">
+            <div className="h-100 p-3 bg-white rounded shadow-sm">
               <CategoryFilter
                 selectedCategories={selectedCategories}
                 setSelectedCategories={setSelectedCategories}
               />
             </div>
           </div>
-          <div className="col-md-8 col-lg-9 d-flex flex-column">
-            <BookList selectedCategories={selectedCategories} />
+
+          {/* Book List Column */}
+          <div className="col-md-8 col-lg-9">
+            <div className="p-3 bg-white rounded shadow-sm">
+              <BookList selectedCategories={selectedCategories} />
+            </div>
           </div>
         </div>
       </div>
